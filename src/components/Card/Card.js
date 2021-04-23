d092cbae2e84e5c688ab57b0c8f7fd059df71799
jeca88 <jeca.petkovic88@gmail.com>
2021-04-23 14:50:19 +0200

    changed style

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,10 +12,10 @@ class Card extends Component {
             {this.props.characters.map(card => {
              return (
                 <div className="card" >
-                <img src={card.image} className="card-img-top" alt="..." 
-                onClick={() => this.props.setCharacter(card.id)}></img>
+                <img src={card.image} className="card-img-top" alt="..." ></img>
                     <div className="card-body">
                         <h5 className="title">{card.name}</h5>
+                        <button className='details' onClick={() => this.props.setCharacter(card.id)}>Details</button>
                     </div>
                 </div>
                 )
